be0243e9003b786889569a214636dad093c09fed
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-01-08 18:22:53 +0100

    add documentation

--- src/Web/Worker/GlobalScope.purs
+++ src/Web/Worker/GlobalScope.purs
@@ -1,3 +1,5 @@
+-- | This module contains functions that can be called by any worker thread.
+
 module Web.Worker.GlobalScope
   ( close
   , location
@@ -23,6 +25,7 @@ foreign import navigator :: Effect Navigator
 
 foreign import postMessageImpl :: forall msg. msg -> Array Transferable -> Effect Unit
 
+-- | sends a message to the main thread that spawned it.
 postMessage :: forall msg. msg -> Effect Unit
 postMessage msg = postMessageImpl msg []
 
@@ -30,10 +33,14 @@ postMessage' :: forall msg. msg -> Array Transferable -> Effect Unit
 postMessage' = postMessageImpl
 
 
+-- | upon calling close, any queued tasks present in the event loop are discarded and the web worker scope is closed.
 foreign import close :: Effect Unit
 
+-- | fired when the worker receives a message from its parent.
 foreign import onMessage :: (MessageEvent -> Effect Unit) -> Effect Unit
 
+-- | fired when a worker receives a message that can't be deserialized.
 foreign import onMessageError :: (MessageEvent -> Effect Unit) -> Effect Unit
 
+-- |  fired when an error occurs in the worker
 foreign import onError :: (Event -> Effect Unit) -> Effect Unit
--- src/Web/Worker/Worker.purs
+++ src/Web/Worker/Worker.purs
@@ -1,3 +1,4 @@
+-- | This module contains function to create and control workers from the parent's worker
 module Web.Worker.Worker
   ( Credentials(..)
   , Worker
@@ -57,6 +58,7 @@ defaultWorkerOptions =
 
 foreign import _new :: String -> { name :: String, credentials :: String, type :: String } -> Effect Worker
 
+-- | creates a worker object that executes the script at the specified URL. 
 new :: String -> WorkerOptions -> Effect Worker
 new url { name, type: t, credentials } =
   _new url
@@ -67,18 +69,24 @@ new url { name, type: t, credentials } =
 
 foreign import postMessageImpl :: forall msg. msg -> Array Transferable -> Worker -> Effect Unit
 
+-- | sends a message to the worker's inner scope. 
 postMessage :: forall msg. msg -> Worker -> Effect Unit
 postMessage msg = postMessageImpl msg []
 
 postMessage' :: forall msg. msg -> Array Transferable -> Worker -> Effect Unit
 postMessage' = postMessageImpl
 
+-- | immediately terminates the worker.
+-- | This does not offer the worker an opportunity to finish its operations; it is stopped at once.
 foreign import terminate :: Worker -> Effect Unit
 
+-- | fired when the worker's parent receives a message from its worker
 foreign import onMessage :: (MessageEvent -> Effect Unit) -> Worker -> Effect Unit
 
+-- | fired when the worker's parent receives a message that can't be deserialized.
 foreign import onMessageError :: (MessageEvent -> Effect Unit) -> Worker -> Effect Unit
 
+-- | fired when an error occurs in the worker.
 foreign import onError :: (Event -> Effect Unit) -> Worker -> Effect Unit
 
 instance Show WorkerType where
