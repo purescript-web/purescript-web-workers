5adbf195a34565e393277930465b921a078d6619
Guillaume Bagan <guillaume.bagan@liris.cnrs.fr>
2023-01-19 19:02:15 +0100

    add new instances for IsSendable

--- spago.dhall
+++ spago.dhall
@@ -4,6 +4,7 @@
   , "foreign"
   , "functions"
   , "maybe"
+  , "newtype"
   , "prelude"
   , "unsafe-coerce"
   , "web-events"
--- src/Web/Worker/Types.purs
+++ src/Web/Worker/Types.purs
@@ -3,10 +3,13 @@ module Web.Worker.Types
   , MessagePort
   , Transferable
   , class IsSendable
-  )
-  where
+  ) where
+
+import Prelude
+
 import Prim.RowList (class RowToList, RowList)
 import Prim.RowList as Row
+import Data.Newtype (class Newtype)
 
 foreign import data Transferable :: Type
 foreign import data MessagePort :: Type
@@ -21,8 +24,24 @@ instance IsSendable Char
 instance IsSendable String
 instance IsSendable a => IsSendable (Array a)
 instance (RowToList r rl, IsSendableRowList rl) => IsSendable (Record r)
+instance IsSendable Unit
+instance IsSendable Void
+instance IsSendable (SendWrapper a)
 
 class IsSendableRowList (rl :: RowList Type)
 
 instance IsSendableRowList Row.Nil
 instance (IsSendable a, IsSendableRowList rest) => IsSendableRowList (Row.Cons sym a rest)
+
+newtype SendWrapper a = SendWrapper a
+
+wrap :: forall a b. Newtype a b => IsSendable b => a -> SendWrapper a
+wrap = SendWrapper
+
+unwrap :: forall a. SendWrapper a -> a
+unwrap (SendWrapper a) = a
+
+-- | Use with care. If you send something that isn't actually Sendable, it
+-- | will raise an exception.
+unsafeWrap :: forall a. a -> SendWrapper a
+unsafeWrap = SendWrapper
